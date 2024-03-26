21a74292b62cee73376fcdc7ba881e196ab50b21
pyooz <real_pyj@naver.com>
2024-03-26 11:11:22 +0900

    줄 정리

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-export default function claw() {
+export default function Claw() {
   return (
     <div className="main">
       <div className="home">
@@ -10,24 +10,14 @@ export default function claw() {
           </div>
           <div className="header-menu">
             <div className="menu">
-              <span className="main-2">
-                공유
-              </span>
-              <span className="main-3">
-                쿠폰
-              </span>
-              <span className="main-4">
-                행사
-              </span>
-              <span className="main-5">
-                시간표
-              </span>
+              <span className="main-2">공유</span>
+              <span className="main-3">쿠폰</span>
+              <span className="main-4">행사</span>
+              <span className="main-5">시간표</span>
             </div>
             <div className="login-group">
               <div className="login">
-                <span className="main-6">
-                  로그인
-                </span>
+                <span className="main-6">로그인</span>
               </div>
             </div>
           </div>
@@ -39,45 +29,36 @@ export default function claw() {
           </div>
           <div className="button">
             <div className="button-contained">
-              <span className="button-9">
-                클로 시작하기
-              </span>
+              <span className="button-9">클로 시작하기</span>
             </div>
           </div>
-          {/* <span className="main-1">
+          {/* 
+          <span className="main-1">
             불편했던 대학 생활을 클로로 정리하자!<br />
             대학 생활을 더욱 효율적으로 관리하고 싶다면?
-          </span> */}
+          </span> 
+          */}
         </div>
       </div>
       <div className="footer">
         <div className="footer-inner">
           <div className="left">
             <div className="header-logo">
-              <span className="claw-7">
-                CLAW
-              </span>
+              <span className="claw-7">CLAW</span>
             </div>
             <div className="address">
-              <span className="main-305">
-                10279 경기도 고양시 덕양구 동헌로 305
-              </span>
+              <span className="main-305">10279 경기도 고양시 덕양구 동헌로 305</span>
             </div>
           </div>
           <div className="top">
-            <span className="main-8">
-              문의하기
-            </span>
+            <span className="main-8">문의하기</span>
           </div>
         </div>
         <div className="frame-1149">
-          <div className="line-8">
-          </div>
-          <span className="copyright">
-            COPYRIGHT ⓒ 2024 CLAW. ALL RIGHTS RESERVED.
-          </span>
+          <div className="line-8"></div>
+          <span className="copyright">COPYRIGHT ⓒ 2024 CLAW. ALL RIGHTS RESERVED.</span>
         </div>
       </div>
     </div>
-  )
+  );
 }
